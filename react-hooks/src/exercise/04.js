0d41a901189e2fb46a7e2863082ce337db488b72
Tom de Bruin <tomdebruin@gmail.com>
2022-01-07 14:11:33 +0000

    excercise 4 extra credit 2

--- react-hooks/src/exercise/04.js
+++ react-hooks/src/exercise/04.js
@@ -2,34 +2,7 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
-
-function useLocalStorageState(
-  key,
-  defaultValue,
-  {serialize = JSON.stringify, deserialise = JSON.parse} = {},
-) {
-  const [state, setState] = React.useState(() => {
-    const localStorageValue = window.localStorage.getItem(key)
-    if (localStorageValue) {
-      return deserialise(window.localStorage.getItem(key))
-    }
-
-    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
-  })
-
-  const prevKeyRef = React.useRef(key)
-
-  React.useEffect(() => {
-    const prevKey = prevKeyRef.current
-    if (prevKey !== key) {
-      window.localStorage.removeItem(prevKey)
-    }
-    prevKeyRef.current = key
-    window.localStorage.setItem(key, serialize(state))
-  }, [key, serialize, state])
-
-  return [state, setState]
-}
+import {useLocalStorageState} from '../utils'
 
 function Board() {
   const newGameArray = Array(9).fill(null)
