4d4b8024738871c1c95b851f5d77c40bb3e12af6
Tom de Bruin <tomdebruin@gmail.com>
2022-01-07 14:09:41 +0000

    excercise 4 extra credit 1

--- react-hooks/src/exercise/04.js
+++ react-hooks/src/exercise/04.js
@@ -3,9 +3,40 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(
+  key,
+  defaultValue,
+  {serialize = JSON.stringify, deserialise = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const localStorageValue = window.localStorage.getItem(key)
+    if (localStorageValue) {
+      return deserialise(window.localStorage.getItem(key))
+    }
+
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
+
+  return [state, setState]
+}
+
 function Board() {
   const newGameArray = Array(9).fill(null)
-  const [squares, setSquares] = React.useState(newGameArray)
+  const [squares, setSquares] = useLocalStorageState(
+    'noughtsandcrosses',
+    newGameArray,
+  )
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
