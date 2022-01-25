70279e27a6cc6531f04ad609e10427b839af574b
Tom de Bruin <tomdebruin@gmail.com>
2022-01-25 16:20:39 +0000

    excercise 7 extra credit 1

--- react-performance/src/exercise/07.js
+++ react-performance/src/exercise/07.js
@@ -2,13 +2,17 @@
 // http://localhost:3000/isolated/exercise/07.js
 
 import * as React from 'react'
+import {unstable_trace as trace} from 'scheduler/tracing'
 // 🐨 you're going to need the reportProfile function
 // 💰 here, let me help you with that...
 import reportProfile from '../report-profile'
 
 function Counter() {
   const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
+  const increment = () =>
+    trace('Increment count', performance.now(), () => {
+      setCount(c => c + 1)
+    })
   return <button onClick={increment}>{count}</button>
 }
 
