f37114515ab33d4bfd6e38f78b98d2790a358a24
Tom de Bruin <tomdebruin@gmail.com>
2022-01-27 14:14:41 +0000

    exercise 1 extra credit 1

--- testing-react-apps/src/__tests__/exercise/01.js
+++ testing-react-apps/src/__tests__/exercise/01.js
@@ -5,6 +5,10 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
   const container = document.createElement('div')
   document.body.append(container)
@@ -18,9 +22,28 @@ test('counter increments and decrements when the buttons are clicked', () => {
   expect(message.textContent).toBe('Current count: 1')
   decrement.click()
   expect(message.textContent).toBe('Current count: 0')
-
-  container.remove()
 })
+
+test('counter increments and decrements when the buttons are clicked with dispatchEvent', () => {
+  const click = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  const container = document.createElement('div')
+  document.body.append(container)
+  ReactDOM.render(<Counter />, container)
+
+  const [decrement, increment] = container.querySelectorAll('button')
+  const message = container.firstChild.querySelector('div')
+
+  expect(message.textContent).toBe('Current count: 0')
+
+  increment.dispatchEvent(click)
+  expect(message.textContent).toBe('Current count: 1')
+
+  decrement.dispatchEvent(click)
+  expect(message.textContent).toBe('Current count: 0')
 })
 
 /* eslint no-unused-vars:0 */
