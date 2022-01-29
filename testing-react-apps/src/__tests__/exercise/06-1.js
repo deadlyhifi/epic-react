25b62673c337fd37239b4e78f57b2154a7001aed
Tom de Bruin <tomdebruin@gmail.com>
2022-01-29 12:00:42 +0000

    excercise 6 extra credit 1

---
+++ testing-react-apps/src/__tests__/exercise/06-1.js
@@ -0,0 +1,46 @@
+// mocking Browser APIs and modules
+// http://localhost:3000/location
+
+import * as React from 'react'
+import {render, screen, act} from '@testing-library/react'
+import {useCurrentPosition} from 'react-use-geolocation'
+import Location from '../../examples/location'
+
+jest.mock('react-use-geolocation')
+
+test('displays the users current location', async () => {
+  const fakePosition = {
+    coords: {
+      latitude: 39,
+      longitude: 1000,
+    },
+  }
+  let setReturnValue
+  function useMockCurrentPosition() {
+    const [state, setState] = React.useState([])
+    setReturnValue = setState
+    return state
+  }
+  useCurrentPosition.mockImplementation(useMockCurrentPosition)
+
+  render(<Location />)
+  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+
+  // Wrapped in act because we're calling a state updater function.
+  act(() => {
+    setReturnValue([fakePosition])
+  })
+
+  expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+  expect(screen.getByText(/latitude/i)).toHaveTextContent(
+    `Latitude: ${fakePosition.coords.latitude}`,
+  )
+  expect(screen.getByText(/longitude/i)).toHaveTextContent(
+    `Longitude: ${fakePosition.coords.longitude}`,
+  )
+})
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
